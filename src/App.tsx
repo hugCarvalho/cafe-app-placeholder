e276a9ada0137283413f930b00215c678c9fae80
Hugo Carvalho <webnoico1978@gmail.com>
2025-05-24 13:26:03 +0200

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is just a placeholder for the cafes app. The code is not public but can be shared during an interview process.
+        </p>
+        <a
+          className="App-link"
+          href="https://cafesandyou.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          To the cafes app
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
